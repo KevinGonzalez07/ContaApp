3a113f198e706e4040b4e7232b07670d51fe0f10
KevinGonzalez07 <kevinglz564@gmail.com>
2026-04-28 16:52:34 -0500

    v2

--- conta-app/src/App.jsx
+++ conta-app/src/App.jsx
@@ -5,7 +5,7 @@ import autoTable from "jspdf-autotable";
 import * as XLSX from "xlsx";
 
 const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL, processLock.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
 
 const ThemeContext = createContext();
